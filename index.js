f98d56775959746131a797b0da77b831cba1b319
Poley Kid <68404606+Compound-codes@users.noreply.github.com>
2023-02-15 17:46:28 -0500

    newest update!
    web ui

--- index.js
+++ index.js
@@ -1,32 +1,54 @@
 // setup
-let firstWord = prompt("First word");
-let secondWord = prompt("Second Word");
+let bias = false;
+
+function biasToggle() {
+    let biasText = $("#bias");
+    if (biasText.html() == "No Bias") {
+        biasText.html("Yes Bias")
+        bias = true;
+        return true;
+    } else {
+        bias = false;
+        biasText.html("No Bias");
+        return false;
+    }
+}
+
+// let firstWord = prompt("First word");
+// let secondWord = prompt("Second Word");
 // fixes maybe sentences to single words
-firstWord = firstWord.toLowerCase();
-secondWord = secondWord.toLowerCase();
-// firstWord = firstWord.split(" ").join("");
-// secondWord = secondWord.split(" ").join("");
-
-let len1 = firstWord.length;
-let len2 = secondWord.length;
-let totalLength = len1 + len2;
-
-let len1fraction = len1/totalLength;
-let len2fraction = len2/totalLength;
-
-let firstWordIndex = Math.round(Math.round(len1fraction*100)/100 * firstWord.length) - 1;
-let secondWordIndex = Math.round(Math.round(len2fraction*100)/100 * secondWord.length) - 1;
-
-let newWord = firstWord.slice(0, firstWordIndex) + secondWord.slice(secondWordIndex);
-console.log(firstWord + secondWord);
-
-console.log(firstWordIndex); 
-console.log(secondWordIndex); 
-
-console.log(newWord);
-
-$("#combined-word").html(newWord);
 
 
+
+
+
+
+function combine() {
+    let firstWord = $("#firstInput").val();
+    let secondWord = $("#secondInput").val();
+    var len1 = firstWord.length;
+    var len2 = secondWord.length;
+    var totalLength = len1 + len2;
+    
+    let len1fraction = len1/totalLength;
+    let len2fraction = len2/totalLength;
+
+    if (bias) {
+        var firstWordIndex = len1 - (Math.round(Math.round(len1fraction*100)/100 * firstWord.length) - 1);
+        var secondWordIndex = len2 - (Math.round(Math.round(len2fraction*100)/100 * secondWord.length) - 1);
+    } else {
+        var firstWordIndex = (Math.round(Math.round(len1fraction*100)/100 * firstWord.length) - 1);
+        var secondWordIndex = (Math.round(Math.round(len2fraction*100)/100 * secondWord.length) - 1);
+    }
+
+
+    var newWord = firstWord.slice(0, firstWordIndex) + secondWord.slice(secondWordIndex);
+    firstWordIndex = len1 - firstWordIndex;
+    secondWordIndex = len2 - secondWordIndex;
+
+    console.log(newWord);
+
+    $("#combined-word").html(newWord);
+}
 
 
